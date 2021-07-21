dca3ebe057c5bd42bbb1aa1ca67476fc92ef7e14
Eliot Szwajkowski <eliotpszw@gmail.com>
2021-07-20 22:32:04 -0400

    basic tech specs done

--- objects/object_mutation_factory/object_mutation_factory.spec.js
+++ objects/object_mutation_factory/object_mutation_factory.spec.js
@@ -9,11 +9,57 @@ describe('objectMutationFactory', () => {
     objectToMutate = objectRefForTest;
   });
 
-  it('Can get and set an object on the factory', () => {
+  it('can get and set an object on the factory', () => {
     objectMutationFactory.setObject(objectToMutate);
 
     const obj = objectMutationFactory.getObject();
 
     expect(obj).toBe(objectToMutate);
+  });
+
+  it('can add a property to an object', () => {
+    objectMutationFactory.setObject(objectToMutate);
+
+    const key = 'dogs';
+    const value = 'are the best animal';
+
+    objectMutationFactory.addProperty(key, value);
+
+    expect(objectToMutate).toEqual({
+      [key]: value,
+    });
+  });
+
+  it('can remove a property from an object', () => {
+    objectMutationFactory.setObject(objectToMutate);
+
+    const key = 'dogs';
+    const value = 'are the best animal';
+
+    objectMutationFactory.addProperty(key, value);
+
+    expect(objectToMutate).toEqual({
+      [key]: value,
+    });
+
+    objectMutationFactory.removeProperty(key);
+
+    expect(objectToMutate).toEqual({});
+  });
+
+  it('can return a cloned version of the object', () => {
+    objectMutationFactory.setObject(objectToMutate);
+
+    const key = 'cats';
+    const value = 'are also pretty cool';
+
+    objectMutationFactory.addProperty(key, value);
+
+    const clonedObj = objectMutationFactory.clone();
+
+    expect(clonedObj).not.toBe(objectToMutate);
+    expect(clonedObj).toEqual({
+      [key]: value,
+    });
   });
 });
